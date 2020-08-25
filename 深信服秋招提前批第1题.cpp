b35cf86e42374c03b9fa2f7d95534d578115937f
ByteLeopord <68319185+ByteLeopord@users.noreply.github.com>
2020-08-25 21:19:13 +0800

    Create 深信服秋招提前批第1题.cpp

---
+++ 深信服秋招提前批第1题.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+//0285289430826
+//2
+//0 2
+//2 3
+string solution(vector<int>& c, string& s){
+	int slen = s.size();
+	for (int i = 0; i < slen; i++){
+		int temp = s[i] - '0';
+		s[i] = c[temp] + '0';
+	}
+	return s;
+}
+
+int main()
+{
+	string s;
+	cin >> s;
+
+	vector<int> mp(10, -1);
+	for (int i = 0; i <= 9; i++){
+		mp[i] = i;
+	}
+
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++){
+		int temp, num;
+		cin >> temp >> num;
+		if (mp[temp] != temp) continue;
+		mp[temp] = num;
+		for (int j = 0; j <= 9; j++){
+			if (mp[j] == temp){
+				mp[j] = num;
+			}
+		}
+	}
+
+	cout << solution(mp, s) << endl;
+	return 0;
+}
