5d5b717bf3b3518a77e8bd54e1fe555edaba19ce
ByteLeopord <68319185+ByteLeopord@users.noreply.github.com>
2020-09-12 22:50:03 +0800

    Create offer68-1二叉搜索树的最近公共祖先.cpp

---
+++ 剑指offer/offer68-1二叉搜索树的最近公共祖先.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // if(root == NULL || p == NULL || q == NULL) return NULL;
+        // if(root == p || root == q) return root;
+        // TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        // TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        // if(left && right) return root;
+        // if(!left) return right;
+        // if(!right) return left;
+        // return NULL;
+        
+        if(root->val < p->val && root->val < q->val)
+            return lowestCommonAncestor(root->right, p, q);
+        if(root->val > p->val && root->val > q->val)
+            return lowestCommonAncestor(root->left, p, q);
+        return root;
+    }
+};
