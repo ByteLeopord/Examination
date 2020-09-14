999309ab208e7fb10b1b749c359276b01c001bd8
ByteLeopord <68319185+ByteLeopord@users.noreply.github.com>
2020-09-14 19:26:06 +0800

    Update offer44数字序列中某一位的数字.cpp

--- 剑指offer/offer44数字序列中某一位的数字.cpp
+++ 剑指offer/offer44数字序列中某一位的数字.cpp
@@ -1,18 +1,27 @@
+/*
+    数字       数量   位数    长度
+    1-9        9      1       9
+    10-99      90     2       180
+    100-999    900    3       2700
+    1000-9999  9000   4       36000
+*/
 class Solution {
 public:
     int findNthDigit(int n) {
         int dig = 1;
         long long base = 9;
+        //计算该数字的位数
         while((n - dig*base) > 0){
             n -= dig*base;
             base *= 10;
             dig++;
         }
-
+        //计算需要获取的位 对应 该数字的第几位
         int index = n % dig;
         if(index == 0){
             index = dig;
         }
+        
         long long num = 1;
         for(int i = 1; i < dig; i++){
             num *= 10;
