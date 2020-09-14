61ea01aa7e66ce60dbebcc3c678f960577b64665
ByteLeopord <68319185+ByteLeopord@users.noreply.github.com>
2020-09-14 20:14:46 +0800

    Update offer53-1在排序数组中查找数字.cpp

--- 剑指offer/offer53-1在排序数组中查找数字.cpp
+++ 剑指offer/offer53-1在排序数组中查找数字.cpp
@@ -1,3 +1,14 @@
+/*
+1.初始化： 左边界 i = 0i=0 ，右边界 j = len(nums) - 1j=len(nums)−1 。
+2.循环二分： 当闭区间 [i, j][i,j] 无元素时跳出；
+    计算中点 m = (i + j) / 2m=(i+j)/2 （向下取整）；
+    若 nums[m] < targetnums[m]<target ，则 targettarget 在闭区间 [m + 1, j][m+1,j] 中，因此执行 i = m + 1i=m+1；
+    若 nums[m] > targetnums[m]>target ，则 targettarget 在闭区间 [i, m - 1][i,m−1] 中，因此执行 j = m - 1j=m−1；
+    若 nums[m] = targetnums[m]=target ，则右边界 rightright 在闭区间 [m+1, j][m+1,j] 中；左边界 leftleft 在闭区间 [i, m-1][i,m−1] 中。因此分为以下两种情况：
+    *若查找 右边界 rightright ，则执行 i = m + 1（跳出时 i 指向右边界）
+    *若查找 左边界 leftleft ，则执行 j = m - 1（跳出时 j 指向左边界）
+3.返回值： 应用两次二分，分别查找 rightright 和 leftleft ，最终返回 right - left - 1right−left−1 即可。
+*/
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
